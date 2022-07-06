5087de73c7b073338d02038e002b92833f525e85
Suman Islam <sumanislam1416@gmail.com>
2022-07-06 15:50:31 +0600

    add graphql route to express server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
 require('dotenv').config();
+
+const schema = require("./schema/schema");
 
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}))
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}...`);
