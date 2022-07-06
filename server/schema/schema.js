13676667ceec9b3e7dfac115b2141c4d339e0dec
Suman Islam <sumanislam1416@gmail.com>
2022-07-06 16:15:56 +0600

    create clients query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,8 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = require('graphql');
 
 const { projects, clients } = require('../sampleData');
@@ -20,6 +21,12 @@ const clientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    clients: {
+     type: new GraphQLList(clientType),
+     resolve(parentValue, args) {
+      return clients;
+     }
+    },
     client: {
       type: clientType,
       args: {id: { type: GraphQLID }},
