4eab7e95913afc4e539fa715d81ad801799f76b4
Suman Islam <sumanislam1416@gmail.com>
2022-07-06 15:44:56 +0600

    create a express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}...`);
+})
