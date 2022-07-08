cea87d96c0e189a53acc2a1304418a16f36b120d
Suman Islam <sumanislam1416@gmail.com>
2022-07-08 23:55:27 +0600

    implement updateProject mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -136,6 +136,39 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, args) {
         return projectModel.findByIdAndDelete(args.id);
       }
+    },
+    updateProject: {
+      type: projectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'ProjectStatusUpdate',
+            values: {
+              new: { value: 'Not Started'},
+              progress: { value: 'In Progress' },
+              completed: { value: 'Completed' }
+            }
+          }) 
+        }
+      },
+      resolve(parentValue, args) {
+        return projectModel.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status
+            }
+          },
+          {
+            new: true
+          }
+        ) 
+      }
     }
 	},
 });
