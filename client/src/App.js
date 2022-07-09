893e80e0e21c8b262b80e66d6c44149215761b59
Suman Islam <sumanislam1416@gmail.com>
2022-07-09 12:28:29 +0600

    create clients and clientRow component and fetch clients from graphql api and display it

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/navbar.component";
+import Clients from "./components/clients.component";
 
 function App() {
   return (
     <>
       <Header />
+      <Clients />
     </>
   );
 }
