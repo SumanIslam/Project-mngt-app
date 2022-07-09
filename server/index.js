0648e47b75a93e059766a9646616e5d56c45939d
Suman Islam <sumanislam1416@gmail.com>
2022-07-09 12:18:36 +0600

    fix cors issue

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 
 require('dotenv').config();
@@ -9,6 +10,8 @@ const { mongoConnect } = require('./config/connectMongo');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
