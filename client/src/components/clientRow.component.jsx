c02ef33b6fdd09f34992f5cf6199b8348141e078
Suman Islam <sumanislam1416@gmail.com>
2022-07-16 18:31:43 +0600

    use refetchQuery instead of update cache

--- client/src/components/clientRow.component.jsx
+++ client/src/components/clientRow.component.jsx
@@ -2,20 +2,21 @@ import { useMutation } from '@apollo/client'
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENT } from '../mutation/clientMutation';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }]
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter(client => client.id !== deleteClient.id)
-        }
-      })
-    }
+    refetchQueries: [{ query: GET_CLIENTS }, {query: GET_PROJECTS}]
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter(client => client.id !== deleteClient.id)
+    //     }
+    //   })
+    // }
   });
 
   return(
