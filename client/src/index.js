c1724461f42a0712d0409d034de8f14d37ad682d
Suman Islam <sumanislam1416@gmail.com>
2022-07-17 21:00:14 +0600

    add some change again

--- client/src/index.js
+++ client/src/index.js
@@ -31,7 +31,7 @@ const cache = new InMemoryCache({
 })
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'http://localhost:5001/graphql',
   cache
 })
 
--- server/index.js
+++ server/index.js
@@ -20,18 +20,18 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 // if (process.env.NODE_ENV === "production") {
-	app.use(express.static('public'));
+	app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(
-			path.resolve(__dirname, "public", "index.html")
+			path.join(__dirname, "..", "client", "build", "index.html")
 		);
 	});
 // }
 
 function startServer() {
   mongoConnect();
-  console.log(path.resolve(__dirname,"..", "public", "index.html"));
+  console.log(path.join(__dirname, "..", "client", "build", "index.html"));
   app.listen(PORT, () => {
 		console.log(`server is running on port ${PORT}...`);
 	});
