677d290b455ed270460a453356019b358ced5cee
Suman Islam <sumanislam1416@gmail.com>
2022-07-17 19:25:20 +0600

    showing static content

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ app.use('/graphql', graphqlHTTP({
   graphiql: process.env.NODE_ENV === 'development'
 }))
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.join(__dirname, "..", "..", "client", "build", "index.html")
+		);
+	});
+}
+
 function startServer() {
   mongoConnect();
 
