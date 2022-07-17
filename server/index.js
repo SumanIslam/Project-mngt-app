75b95eb29708fab044dc3d6b9ba26329e9710d6f
Suman Islam <sumanislam1416@gmail.com>
2022-07-17 19:29:29 +0600

    add some change in path.join

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
+	app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(
-			path.join(__dirname, "..", "..", "client", "build", "index.html")
+			path.join(__dirname, "..", "client", "build", "index.html")
 		);
 	});
 }
